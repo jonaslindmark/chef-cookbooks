8dc8a1532637823945242abe4ce7290ca390af70
Jonas Lindmark <lindmark@wrapp.com>
2013-03-25 21:20:33 +0100

    Added nodedev role

---
+++ roles/nodedev.rb
@@ -0,0 +1,14 @@
+name "nodedev"
+description "Set up a devbox"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+run_list(
+  "recipe[apt]",
+  "recipe[nodejs]"
+)
+# Attributes applied if the node doesn't have it set already.
+override_attributes 'nodejs' => {
+    'version' => '0.10.1'
+}
+default_attributes()
+# Attributes applied no matter what the node has set already.
+#override_attributes()
